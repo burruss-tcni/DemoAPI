1aeaabf3421dfb25114d4ccee82f7f7183115b04
burruss-tcni <73897510+burruss-tcni@users.noreply.github.com>
2021-07-01 16:30:50 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,18 @@
+# importing os module
+import os
+import sys
+     
+# Get the current working
+# directory (CWD)
+cwd = os.getcwd()
+
+def lambda_handler(event, context):
+    #1 - log the event
+    print('\n*************This is the event and info**********\n')
+    print(event)
+    
+    # Print the current working
+    # directory (CWD)
+    print("Current working directory: ", cwd)
+    print('\n*******This is the end of the code info***********\n')
+    # print(event)
